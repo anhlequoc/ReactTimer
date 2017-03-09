0f394af8b57763f82d154ddd82f5547e962986dd
Anh Le <lequocanh88@gmail.com>
2017-03-09 23:08:42 +0700

    start rendering timer from same Control module

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -3,20 +3,33 @@ var React = require('react');
 var Controls = React.createClass({
   propTypes: {
     countdownStatus: React.PropTypes.string.isRequired,
-    onStatusChange: React.PropTypes.func.isRequired
+    //onStatusChange: React.PropTypes.func.isRequired,
+    timerStatus: React.PropTypes.string.isRequired,
+    //onTimerStatusChange: React.PropTypes.func.isRequired
   },
   onStatusChange: function(newStatus) { {/*use function to return a function -> define new status right inside function called, no need to have 3 separated functions*/}
     return () => {
       this.props.onStatusChange(newStatus);
     };
   },
+  onTimerStatusChange: function(newStatus) {
+    return () => {
+      this.props.onTimerStatusChange(newStatus);
+    };
+  },
   render: function() {
-    var {countdownStatus} = this.props;
+    var {countdownStatus, timerStatus} = this.props;
     var renderStartStopButton = () => {
       if (countdownStatus === 'started') {
         return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>;
       } else if (countdownStatus === 'paused'){
         return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>;
+      }
+
+      if(timerStatus === 'started') {
+        return <button className="button secondary" onClick={this.onTimerStatusChange('stopped')}>Stop</button>;
+      } else if (timerStatus === 'stopped') {
+        return <button className="button secondary" onClick={this.onTimerStatusChange('started')}>Start</button>;
       }
     };
 
--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,10 +1,25 @@
 var React = require('react');
+var Controls = require('Controls');
+var Clock = require('Clock');
 
 var Timer = React.createClass({
+  getInitialState: function () {
+    return {
+      timerStatus: 'stopped'
+    };
+  },
+  handleTimerStatusChange: function(newStatus) {
+    this.setState({
+      timerStatus: newStatus
+    });
+  },
   render: function(){
+    var {timerStatus, countdownStatus} = this.state;
     return (
       <div>
-        <p>Timer.jsx rendered</p>
+        <h1>Timer App</h1>
+        <Clock />
+        <Controls timerStatus={timerStatus} countdownStatus={''} onTimerStatusChange={this.handleTimerStatusChange}/>
       </div>
     );
   }
